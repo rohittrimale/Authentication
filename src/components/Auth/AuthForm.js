ed7c3ececd4fed7754864ab9c57cbb01b016047b
rohittrimale <rohittrimalle@gmail.com>
2024-05-25 15:30:21 +0530

    Create accout and alos sign i n

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,105 @@
+import { useState, useRef, useContext } from "react";
+
+import classes from "./AuthForm.module.css";
+import { AuthContext } from "../../store/auth-context";
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const authContext = useContext(AuthContext);
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    setIsLoading(true);
+    let url;
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDkzN10Vvdn3JGR_tME3ZfwgVvJKp3mLMs";
+    } else {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDkzN10Vvdn3JGR_tME3ZfwgVvJKp3mLMs`;
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+
+        if (res.ok) {
+          return res.json();
+        } else {
+          res.json().then((data) => {
+            let errorMessage = "Authtication failed";
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+            alert(errorMessage);
+            // throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        authContext.login(data.idToken);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          {!isLoading ? (
+            <button type="submit" className={classes.button}>
+              {isLogin ? "Log In" : "Create new Account"}
+            </button>
+          ) : (
+            <button className={classes.button}>Loading ......</button>
+          )}
+        </div>
+
+        <div className={classes.actions}>
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
